8b3dd7c49ca7e4677dd95f520d302fa469c30d2b
Patryk Pjanka <ppjanka@princeton.edu>
2021-04-07 15:18:44 +0200

    Added join_lis.sh for more automatic joining of MPI-generated lis files. Some adjustments to join_vtk.sh as well.

--- vis/vtk/join_vtk.sh
+++ vis/vtk/join_vtk.sh
@@ -14,19 +14,19 @@ if [ ! -f ../vis/vtk/join_vtk ]; then
 fi
 
 # join the vtk files
-rm -r $1/joined
-mkdir $1/joined
+rm -r $1/joined_vtk
+mkdir $1/joined_vtk
 nlevels=$(ls -d $1/id0/lev* | wc -l)
 for filepath in $1/id0/*.vtk
 do
     filename=$(basename $filepath)
     fileno=$(echo $filename | awk '{split($0,words,"."); print words[2];}')
     echo $fileno
-    ../vis/vtk/join_vtk -o $1/joined/$filename $1/id*/*$fileno.vtk
+    ../vis/vtk/join_vtk -o $1/joined_vtk/$filename $1/id*/*$fileno.vtk
     for level in $(seq 1 $nlevels)
     do
-        mkdir $1/joined/lev$level
-        ../vis/vtk/join_vtk -o $1/joined/lev$level/$filename $1/id*/lev$level/*$fileno.vtk
+        mkdir $1/joined_vtk/lev$level
+        ../vis/vtk/join_vtk -o $1/joined_vtk/lev$level/$filename $1/id*/lev$level/*$fileno.vtk
     done
 done
 rm $1/id*/*.vtk
