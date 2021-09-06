43b03ea9096ac5919be0e7ec3df599b00417e2eb
Patryk Pjanka <ppjanka@princeton.edu>
2021-09-06 17:12:13 +0200

    join_vtk now checks for file existance before joining.

--- bin_paper1/join_vtk.sh
+++ bin_paper1/join_vtk.sh
@@ -21,14 +21,16 @@ nlevels=$(ls -d $1/id0/lev* | wc -l)
 for filepath in $1/id0/*.vtk
 do
     filename=$(basename $filepath)
-    fileno=$(echo $filename | awk '{split($0,words,"."); print words[2];}')
-    echo $fileno
-    ../vis/vtk/join_vtk -o $1/joined_vtk/$filename $1/id*/*$fileno.vtk
-    for level in $(seq 1 $nlevels)
-    do
-        mkdir $1/joined_vtk/lev$level
-        ../vis/vtk/join_vtk -o $1/joined_vtk/lev$level/$filename $1/id*/lev$level/*$fileno.vtk
-    done
+    if [ ! -f $1/joined_vtk/lev$nlevels/$filename ]; then
+        fileno=$(echo $filename | awk '{split($0,words,"."); print words[2];}')
+        echo $fileno
+        ../vis/vtk/join_vtk -o $1/joined_vtk/$filename $1/id*/*$fileno.vtk
+        for level in $(seq 1 $nlevels)
+        do
+            mkdir $1/joined_vtk/lev$level
+            ../vis/vtk/join_vtk -o $1/joined_vtk/lev$level/$filename $1/id*/lev$level/*$fileno.vtk
+        done
+    fi
 done
 
 # remove the fragmented data
