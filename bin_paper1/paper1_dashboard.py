cc2449ecf14b3f95e00cd0468ad7756fbdc8c98c
Patryk Pjanka <patryk.pjanka@gmail.com>
2022-10-25 18:44:04 +0200

    Ready for blazar processing.

--- bin_paper1/paper1_dashboard.py
+++ bin_paper1/paper1_dashboard.py
@@ -347,12 +347,12 @@ nu_int_min = 300.0e9
 nu_int_max = 3.0e15
 
 # system properties (see overleaf)
-gamma_jet = 2.0
-beta_jet = np.sqrt(1.0 - 1.0/gamma_jet**2) # old bug: 0.75
-incl = 30.0 * np.pi / 180.
-theta_j = 2.3 * np.pi / 180.
+gamma_jet = 10.0 # 2.0
+beta_jet = np.sqrt(1.0 - 1.0/gamma_jet**2)
+incl = 1./gamma_jet # 30.0 * np.pi / 180.
+# theta_j = 2.3 * np.pi / 180.
 dist = 8. * kpc
-mbh = 10. * Msun
+# mbh = 10. * Msun
 
 
 # In[6]:
